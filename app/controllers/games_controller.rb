c4785c7a146e2c18946c8a92f48b1df93918bdc4
Mateusz Buczek <mtsbck@gmail.com>
2022-08-12 22:17:24 +0200

    latest release version kek

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,9 +6,9 @@ class GamesController < ApplicationController
     @match = Pvp.new(players: 2, board_size: 8, uniform: false, enable_randomness: false)
     @board = @match.game.board
     @game = @match.game
-    # @game.place(owner: 'Player1', type: 'skeleton', x: 0, y: 0, board_fields:  @board)
-    # @game.place(owner: 'Player1', type: 'skeleton archer', x: 1, y: 1, board_fields:  @board)
-    # @game.place(owner: 'Player1', type: 'skeleton', x: 0, y: 2, board_fields:  @board)
-    # @game.place(owner: 'Player2', type: 'skeleton archer', x: 4, y: 7, board_fields:  @board)
+    @game.place(owner: 'Player1', type: 'skeleton', x: 0, y: 0, board_fields:  @board)
+    @game.place(owner: 'Player1', type: 'skeleton archer', x: 1, y: 1, board_fields:  @board)
+    @game.place(owner: 'Player1', type: 'skeleton', x: 0, y: 2, board_fields:  @board)
+    @game.place(owner: 'Player2', type: 'skeleton archer', x: 4, y: 7, board_fields:  @board)
   end
 end
