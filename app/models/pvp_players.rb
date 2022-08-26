40a79e5a53acc1d65cbc35fd94ab5d7a4d2b706c
Mateusz Buczek <mtsbck@gmail.com>
2022-08-26 17:38:51 +0200

    Web alpha: fixed players starting with 2 mana which was used for testing purposes

--- app/models/pvp_players.rb
+++ app/models/pvp_players.rb
@@ -4,7 +4,7 @@ class PvpPlayers < ApplicationRecord
   # validates :board, presence: true
   def self.add_player(game_id)
     # if PvpPlayers.where(game_id: game_id).size < 4
-    added_player = PvpPlayers.new(name: "Player#{PvpPlayers.all.size + 1}", mana: 2, max_mana: 10,
+    added_player = PvpPlayers.new(name: "Player#{PvpPlayers.all.size + 1}", mana: 10, max_mana: 10,
                                   summoning_zone: '', game_id: game_id, color: pick_color)
     added_player.save
     Game.add_player(game_id: game_id, player_id: added_player.id)
