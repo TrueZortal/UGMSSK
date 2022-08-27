d3f4ecfa51b030f64dcb806cb56de71ac6e28c31
Mateusz Buczek <mtsbck@gmail.com>
2022-08-27 16:47:36 +0200

    Web alpha: identifying drag and drop actions

--- app/javascript/controllers/minion_controller.js
+++ app/javascript/controllers/minion_controller.js
@@ -3,27 +3,43 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   // static targets = ["minion"]
 
-  setListeners() {
-  let items = document.querySelectorAll('minion');
-  items.forEach(function (item) {
-    console.log("this triggered for "+item)
-    item.addEventListener('dragstart', handleDragStart);
-    item.addEventListener('dragend', handleDragEnd);
-  })
-}
+  handleDragStart(event) {
+    console.log("drag start")
+    console.log(event)
+    let id = this.element.getAttribute("occupant_id")
+    let minion = fetch('/summoned_minions/'+id+'/grab/')
+    .then((response) => response.json())
 
-handleDragStart(e) {
-  console.log("drag start")
-  this.style.opacity = '0.4';
-}
+    console.log(minion)
+    // this.style.opacity = '0.4';
+  }
 
-handleDragEnd(e) {
-  console.log("drag end")
-  this.style.opacity = '1';
-}
+  handleDragEnd(event) {
+    console.log("drag end")
+    console.log(event)
 
-  getId() {
-    // console.log(e)
+    // this.style.opacity = '1';
+  }
+
+  connect() {
+  }
+
+  // disconnect() {
+    //   let items = document.getElementsByClassName("minion")
+    //   console.log("this works but something is missing")
+    //   // console.log(items)
+    //   Array.from(items).forEach(function (item) {
+      //     console.log("this triggered for "+item)
+      //     item.removeEventListener('dragstart', this.handleDragStart.bind(this))
+      //     item.removeEventListener('dragend', this.handleDragEnd.bind(this))
+      //   })
+      // }
+
+      getId() {
+      this.element.addEventListener('dragstart', this.handleDragStart.bind(this))
+      this.element.addEventListener('dragend', this.handleDragEnd.bind(this))
+        // console.log(e)
+
     let id = this.element.getAttribute("occupant_id")
     let minion = fetch('/summoned_minions/'+id+'/grab/')
     .then((response) => response.json())
